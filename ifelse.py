9d1badc9edd789b05f832160048c68c2c7b12f2d
timur-79 <megabyte@tut.by>
2020-12-25 09:52:07 +0300

    if-else for

--- ifelse.py
+++ ifelse.py
@@ -6,14 +6,12 @@ if x > 85:
     print('Pass')
 else:
     print('No pasaran')
-
-for num in (10, 2, -17, 2, -43, 11, 22.5):
+A=(10, 2, -17, 2, -43, 11, 22.5)
+for num in A:
     if num % 2 == 0:
         print('Четное ', num)
     elif num % 2 == 1:
         print('Нечетное ', num)
     else:
         print('Чо за нах?')
-y=78
-y=-y
-print(-y)
+
