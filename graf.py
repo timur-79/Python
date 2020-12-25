7da6f8133af8d2f24bc1af85b15c74ee5922c52c
timur-79 <megabyte@tut.by>
2020-12-25 09:09:29 +0300

    if-else for

--- graf.py
+++ graf.py
@@ -2,5 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 x = np.arange(-10,10.01, 0.01)
 plt.plot(x,x**2)
-plt.plot(x, np.sin(x), np.cos(x))
+plt.plot(x, np.sin(x)*10)
 plt.show()
---
+++ ifelse.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+x = np.random.rand() * 100
+type(x)
+if x > 85:
+    print('Pass')
+else:
+    print('No pasaran')
+
+for num in (10, 2, -17, 2, -43, 11, 22.5):
+    if num % 2 == 0:
+        print('Четное ', num)
+    elif num % 2 == 1:
+        print('Нечетное ', num)
+    else:
+        print('Чо за нах?')
